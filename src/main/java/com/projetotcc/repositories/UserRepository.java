9441e2f2e21c3538e670c227a81b3e7895458458
GabrielaSodre <gabi.silvasodre@hotmail.com>
2026-08-23 16:43:20 -0300

    feat: criando repositório de usuário

---
+++ src/main/java/com/projetotcc/repositories/UserRepository.java
@@ -0,0 +1,13 @@
+package com.projetotcc.repositories;
+
+import com.projetotcc.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    Optional<User> findUserByDocument(String document);
+
+    Optional<User> findUserbyId(Long id);
+
+}
